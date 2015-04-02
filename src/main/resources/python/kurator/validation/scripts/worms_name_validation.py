c11fc72901993b97040c82a80c471e3537368a91
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-04-02 16:03:41 -0700

    Updates to reflect changes in WoRMS actor.

--- src/main/resources/python/kurator/validation/scripts/worms_name_validation.py
+++ src/main/resources/python/kurator/validation/scripts/worms_name_validation.py
@@ -3,18 +3,15 @@ import sys
 import csv
 
 sys.path.append('/Users/tmcphill/GitRepos/kurator-validation/src/main/resources/python')
-from kurator.validation.actors.WoRMSNameLookup import start as start_actor
-from kurator.validation.actors.WoRMSNameLookup import validate as validate_record
+from kurator.validation.actors import WoRMSActor
 
-inputfile = "/Users/tmcphill/GitRepos/kurator-validation/src/test/resources/org/kurator/validation/data/testinput_moll.csv"
+inputfile = '/Users/tmcphill/GitRepos/kurator-validation/src/test/resources/org/kurator/validation/data/testinput_moll.csv'
 
-data = []
-with open(inputfile, "r") as infile, open('out.csv', 'w') as outfile:
-  dr = csv.DictReader(infile)
-  dw = csv.DictWriter(outfile, ['ID','TaxonName','Author','OriginalName','OriginalAuthor','lsid'])
-  dw.writeheader()
-  start_actor()
-  for record in dr:
-      validate_record(record)
-      dw.writerow(record)
-    
+with open(inputfile, 'r') as infile, open('out.csv', 'w') as outfile:
+    dr = csv.DictReader(infile)
+    dw = csv.DictWriter(outfile, ["ID", 'TaxonName', 'Author', 'OriginalName', 'OriginalAuthor', 'lsid'])
+    dw.writeheader()
+    WoRMSActor.start()
+    for record in dr:
+        WoRMSActor.curate(record)
+        dw.writerow(record)
