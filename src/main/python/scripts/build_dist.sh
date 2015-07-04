660584a6ea3bb1d49e780505add03d9fbe8ffa4f
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-07-04 15:48:49 -0700

    Updated distribution build script to execute in Bamboo.

--- src/main/python/scripts/build_dist.sh
+++ src/main/python/scripts/build_dist.sh
@@ -1,3 +1,17 @@
+# install pip in a temporary directory
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --root=${bamboo.build.working.directory}/tmp --ignore-installed
+export PATH=${bamboo.build.working.directory}/tmp/usr/local/bin:$PATH
+export PYTHONPATH=${bamboo.build.working.directory}/tmp/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
+
+# install and activate a virtualenv environment into which wheel can be installed via pip
+pip install --root=${bamboo.build.working.directory}/tmp --ignore-installed virtualenv
+virtualenv virtual_tmp
+. virtual_tmp/bin/activate
+
+# install the wheel package
 pip install wheel
-cd src/main/resources/python
+
+# build a wheel distribution of kurator-validator python code
+cd src/main/python
 python setup.py bdist_wheel
