abfca6530e62056fc33d41e4b5a779a8ce1f9c2f
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-07-04 16:08:41 -0700

    Now using variable to define local TMP directory.

--- src/main/python/scripts/build_dist.sh
+++ src/main/python/scripts/build_dist.sh
@@ -1,11 +1,13 @@
+TMPDIR=${bamboo.build.working.directory}/tmp
+
 # install pip in a temporary directory
 wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --root=${bamboo.build.working.directory}/tmp --ignore-installed
-export PATH=${bamboo.build.working.directory}/tmp/usr/local/bin:$PATH
-export PYTHONPATH=${bamboo.build.working.directory}/tmp/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
+python get-pip.py --root=$TMPDIR --ignore-installed
+export PATH=$TMPDIR/usr/local/bin:$PATH
+export PYTHONPATH=$TMPDIR/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
 
 # install and activate a virtualenv environment into which wheel can be installed via pip
-pip install --root=${bamboo.build.working.directory}/tmp --ignore-installed virtualenv
+pip install --root=$TMPDIR --ignore-installed virtualenv
 virtualenv virtual_tmp
 . virtual_tmp/bin/activate
 
