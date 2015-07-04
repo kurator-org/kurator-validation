10ed6bc1a2aef8783169224b1ecf8f97e64ef549
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-07-04 16:20:35 -0700

    Moved build_dist script into main python directory.

--- src/main/python/scripts/setup_pip.sh
+++
@@ -1,8 +0,0 @@
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --root=${bamboo.build.working.directory}/tmp --ignore-installed
-export PATH=${bamboo.build.working.directory}/tmp/usr/local/bin:$PATH
-export PYTHONPATH=${bamboo.build.working.directory}/tmp/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
-pip install --root=${bamboo.build.working.directory}/tmp --ignore-installed virtualenv
-virtualenv virtual_tmp
-cd virtual_tmp
-. bin/activate
