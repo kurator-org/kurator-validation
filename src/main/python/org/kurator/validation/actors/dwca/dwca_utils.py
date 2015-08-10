aad6a532414e8e6694439400ee0ee574ecdbb60d
tucotuco <tuco@berkeley.edu>
2015-08-10 23:04:28 +0200

    Cleanup of vocabulary management and first forays into the GBIFResultReader.

--- src/main/python/org/kurator/validation/actors/dwca/dwca_utils.py
+++ src/main/python/org/kurator/validation/actors/dwca/dwca_utils.py
@@ -26,19 +26,63 @@ import csv
 # pip install python-dwca-reader
 
 from dwca.read import DwCAReader
+from dwca.read import GBIFResultsReader
 from dwca.darwincore.utils import qualname as qn
 from dwca.darwincore.terms import TERMS
 from dwcaterms import geogkeytermlist
 from dwcaterms import taxonkeytermlist
 
-# geogkeytermlist = ['continent', 'country', 'stateProvince', 'county', 'municipality', 'waterBody', 'islandGroup', 'island']
-# taxonkeytermlist = ['kingdom', 'genus', 'subgenus', 'specificEpithet', 'infraspecificEpithet', 'scientificNameAuthorship', 'scientificName']
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+        
+def standardize_term(dwcareader, changeterm, lookupdict):
+    if dwcareader is None or changeterm is None or lookupdict is None:
+        return None
+        
+    newvalues=set()
+    for row in dwcareader:
+        shouldbe=None
+        was=get_term_value(row.data, changeterm)
+        if was not in newvalues:
+            shouldbe=get_standard_value(was, lookupdict)
+            if shouldbe is None:
+                newvalues.add(was)
+            elif was!=shouldbe:
+                set_term_value(row.data, changeterm, shouldbe)
+    return newvalues
+
+def append_to_vocab(filename, newtermlist):
+    """Add values to a lookup dictionary file."""
+    dialect = None
+    with open(filename, 'rb') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+    dialect.lineterminator='\r'
+
+    with open(filename, 'a') as csvfile:
+        fieldnames=['verbatim','standard','checked']
+        writer = csv.DictWriter(csvfile, dialect=dialect, quoting=csv.QUOTE_NONNUMERIC, fieldnames=fieldnames)
+        for term in newtermlist:
+            if term is not None:
+                writer.writerow({'verbatim':term, 'standard':'', 'checked':0 })
 
 def get_dict_for_vocab(filename):
     """Create a lookup dictionary for standard values of a term."""
     dict={}
+    dialect = None
+    with open(filename, 'rb') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+    dialect.lineterminator='\r'
     with open(filename) as csvfile:
-        dr = csv.DictReader(csvfile)
+        dr = csv.DictReader(csvfile, dialect=dialect, quoting=csv.QUOTE_NONNUMERIC)
         for row in dr:
             verbatim=row['verbatim']
             standard=row['standard']
@@ -78,8 +122,6 @@ def get_term_valueset(dwcareader, term):
     # Iterate over the archive core rows to count them
     for row in dwcareader:
         v = get_term_value(row.data, term)
-        if v is None:
-            v = get_term_value(row.data, qn(term))
         valueset.add(v)
     return valueset
 
@@ -91,18 +133,17 @@ def get_term_group_key(rowdata, term_list):
         return None
     key = ''
     for term in term_list:
+        # Try to find the value of the term in the rowdata, even using the fully 
+        # qualified version of the term name.
         v = get_term_value(rowdata, term)
         if v is None:
-            try:
-                q = qn(term)
-                v = get_term_value(rowdata, q)
-            except Exception, e:
-                logging.error('%s is not a Simple Darwin Core term. The search is case-sensitive.' % (term))
-        if v is None:
+            # Could not find the term in the rowdata, even using the qualified name.
             v = ''
         if key == '':
+            # If this is the first term in the key group.
             key = v
         else:
+            # For subsequent terms in the key group
             key = key + ' | ' + v
     return key 
 
@@ -115,7 +156,7 @@ def archive_has_core_term(dwcareader, term):
     try:
         q = qn(term)
     except Exception, e:
-        logging.error('%s is not a Simple Darwin Core term. The search is case-sensitive.' % (term))
+        logging.error('archive_has_core_term(): %s is not a Simple Darwin Core term. The search is case-sensitive.' % (term))
         return False
     if q in dwcareader.descriptor.core.terms:
         return True
@@ -150,34 +191,59 @@ def row_has_term_value(rowdata, term):
             return True
     return False
     
+def set_term_value(rowdata, term, value):
+    """Set the value of the term in the given rowdata."""
+    if rowdata is None:
+        return
+    if term in rowdata.keys():
+        rowdata[term]=value
+    elif qn(term) in rowdata.keys():
+        rowdata[qn(term)]=value
+    return
+
 def get_term_value(rowdata, term):
     """Return the value of the term in the given rowdata."""
     if rowdata is None:
         return None
     if term in rowdata.keys():
         return rowdata[term]
+    # Try a Darwin Core fully qualified term if it wasn't found as is.
+    try:
+        q=qn(term)
+    except Exception, e:
+        return None
+    if q in rowdata.keys():
+        return rowdata[q]
     return None
-    
-def standardize_term_value(rowdata, term, valuedict):
-    """Replace the value of the term in rowdata with the value from the valuedict."""
-    was=None
-    shouldbe=None
-    if rowdata is None:
-        return False
-    if term is None:
-        return False
-    if term in rowdata.keys():
-        was=rowdata[term]
-    elif qn(term) in rowdata.keys():
-        was=rowdata[qn(term)]
-    if was is not None:
-    	shouldbe=valuedict[was]
-    	if was!=shouldbe:
-            rowdata[term]=shouldbe
-            print 'was: %s shouldbe: %s' % (was, shouldbe)
-            return True
-    return False
-    
+
+def get_standard_value(was, valuedict):
+    """Return a standard value from the valuedict."""
+    if was is None:
+        return None
+    if was in valuedict.keys():
+        return valuedict[was]
+    return None
+
+# def standardize_term_value(rowdata, term, valuedict):
+#     """Replace the value of the term in rowdata with the value from the valuedict."""
+#     was=None
+#     shouldbe=None
+#     if rowdata is None:
+#         return False
+#     if term is None:
+#         return False
+#     if term in rowdata.keys():
+#         was=rowdata[term]
+#     elif qn(term) in rowdata.keys():
+#         was=rowdata[qn(term)]
+#     if was is not None:
+#     	shouldbe=valuedict[was]
+#     	if was!=shouldbe:
+#             rowdata[term]=shouldbe
+#             print 'was: %s shouldbe: %s' % (was, shouldbe)
+#             return True
+#     return False
+#     
 def shortname(qualname):
     """Return a term name from a fully qualified term identifier.
 
@@ -206,6 +272,9 @@ def _getoptions():
     parser.add_option("-f", "--dwca_file", dest="dwca_file",
                       help="Darwin Core Archive file",
                       default=None)
+    parser.add_option("-t", "--archive_type", dest="archive_type",
+                      help="Darwin Core Archive file type. None or 'gbif'",
+                      default=None)
     return parser.parse_args()[0]
 
 def main():
@@ -214,7 +283,17 @@ def main():
     if options.dwca_file is None:
         print 'syntax: dwca_utils.py -f dwca_file'
         return
-    dwcareader = DwCAReader(options.dwca_file)
+    dwcareader = None
+    if options.archive_type=='gbif':
+        dwcareader = GBIFResultsReader(options.dwca_file)
+    else:
+        dwcareader = DwCAReader(options.dwca_file)
+    if dwcareader is None:
+        print 'No archive found at %s' % options.dwca_file
+
+    rowcount = get_core_rowcount(dwcareader)
+    print '\nCore row count:%s' % (rowcount)
+
     metadata=dwca_metadata(dwcareader)
     print 'Metadata:\n%s' % metadata
 
@@ -242,32 +321,45 @@ def main():
         print '%s' % (taxonkey)
     print 'Count=%s' % i
 
-    i = 0
-    changed=0
-    changeterm='establishmentMeans'
-    em_dict=get_dict_for_vocab('../../vocabularies/establishmentMeans.csv')
-    print 'establishmentMeans dict:\n %s' % em_dict
-    for row in dwcareader:
-        if standardize_term_value(row.data, changeterm, em_dict) is True:
-            changed=changed+1
-        i = i + 1
-    print 'Count=%s %s changed %s times' % (i, changeterm, changed)
-    
-    i = 0
-    changed=0
+#     i = 0
+#     changed=0
+#     changeterm='establishmentMeans'
+#     em_dict=get_dict_for_vocab('../../vocabularies/establishmentMeans.csv')
+#     print 'establishmentMeans dict:\n %s' % em_dict
+#     for row in dwcareader:
+#         if standardize_term_value(row.data, changeterm, em_dict) is True:
+#             changed=changed+1
+#         i = i + 1
+#     print 'Count=%s %s changed %s times' % (i, changeterm, changed)
+#     
+#     i = 0
+#     changed=0
+#     changeterm='basisOfRecord'
+#     bor_dict=get_dict_for_vocab('../../vocabularies/basisOfRecord.csv')
+#     print 'basisOfRecord dict:\n %s' % bor_dict
+#     for row in dwcareader:
+#         if standardize_term_value(row.data, changeterm, bor_dict) is True:
+#             changed=changed+1
+#         i = i + 1
+#     print 'Count=%s %s changed %s times' % (i, changeterm, changed)
+
     changeterm='basisOfRecord'
-    bor_dict=get_dict_for_vocab('../../vocabularies/basisOfRecord.csv')
-    print 'basisOfRecord dict:\n %s' % bor_dict
-    for row in dwcareader:
-        if standardize_term_value(row.data, changeterm, bor_dict) is True:
-            changed=changed+1
-        i = i + 1
-    print 'Count=%s %s changed %s times' % (i, changeterm, changed)
+    vocab_file='../../vocabularies/basisOfRecord.csv'
+    vdict=get_dict_for_vocab(vocab_file)
+    newvalues=standardize_term(dwcareader, changeterm, vdict)
+    print 'Append these to %s newvalues=%s' % (vocab_file, newvalues)
+    if newvalues is not None:
+        append_to_vocab(vocab_file, newvalues)
+
+    changeterm='institutionCode'
+    vocab_file='../../vocabularies/icode.csv'
+    vdict=get_dict_for_vocab(vocab_file)
+    newvalues=standardize_term(dwcareader, changeterm, vdict)
+    print 'Append these to %s newvalues=%s' % (vocab_file, newvalues)
+    if newvalues is not None:
+        append_to_vocab(vocab_file, newvalues)
     
     dwcareader.close()
-
-    rowcount = get_core_rowcount(dwcareader)
-    print '\nCore row count:%s' % (rowcount)
 
 if __name__ == '__main__':
     """ Demo of dwca_utils functions"""
---
+++ src/main/python/org/kurator/validation/data/readme.txt
@@ -0,0 +1,5 @@
+Files in this directory include:
+
+dwca-uwymv_herp.zip - a Darwin Core archive with eight occurrence records downloaded from an instance of an Integrated Publishing Toolkit. Source: http://ipt.vertnet.org:8080/ipt/resource.do?r=uwymv_herp accessed 28 Jul 2015.
+eight_specimen_records.csv - a CSV sample Simple Darwin Core occurrence file of eight records.
+GBIFLemurs.zip - an archive of 4783 occurrences downloaded from the results of a query for scientific name 'Primates' and country 'Madagascar on the GBIF portal. Source: http://www.gbif.org/occurrence/search?TAXON_KEY=798&COUNTRY=MG, accessed 10 Aug 2015.
