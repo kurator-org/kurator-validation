f8a1b66a2d8adae6ccb1cc7770bc43fc40b1adea
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-03-09 14:06:07 -0800

    Added package build scripts for kurator_worms Python package.

---
+++ packages/kurator_worms/build_dist_on_bamboo.sh
@@ -0,0 +1,22 @@
+#################  Script task configuration in Bamboo #################
+#  Script file:           packages/kurator_worms/build_dist_on_bamboo.sh
+#  Environment variables: TMPDIR=${bamboo.build.working.directory}/tmp
+########################################################################
+
+# install pip in a temporary directory
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --root=$TMPDIR --ignore-installed
+export PATH=$TMPDIR/usr/local/bin:$PATH
+export PYTHONPATH=$TMPDIR/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
+
+# install and activate a virtualenv environment into which wheel can be installed via pip
+pip install --root=$TMPDIR --ignore-installed virtualenv
+virtualenv virtual_tmp
+. virtual_tmp/bin/activate
+
+# install the wheel package
+pip install wheel
+
+# build a wheel distribution of kurator-validator python code
+cd packages/kurator_worms
+python setup.py bdist_wheel
