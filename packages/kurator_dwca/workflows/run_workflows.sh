10978e645a5679f6c3130d6fa3688b9358b30633
tucotuco <tuco@berkeley.edu>
2016-10-13 01:42:35 +0200

    Added versions of term value assessment workflows that work on specific files rather than a Darwin Core Archive from a URL.

--- packages/kurator_dwca/workflows/run_workflows.sh
+++ packages/kurator_dwca/workflows/run_workflows.sh
@@ -11,3 +11,4 @@ ka -f file_aggregator.yaml -p workspace=./file_aggregator_workspace -p inputfile
 
 ka -f dwca_geography_assessor.yaml -p workspace=./ws_geography_assessor -p dwca_url=http://ipt.vertnet.org:8080/iptstrays/archive.do?r=mcnb-chordata
 ka -f file_geography_cleaner.yaml -p workspace=./file_geography_cleaner_workspace -p inputfile=../data/tests/test_barcelona1.txt -p outputfile=cleaned_geography_file.csv -p format=csv
+ka -f file_term_values.yaml -p workspace=./file_term_values_workspace -p inputfile=../data/tests/test_barcelona1.txt -p format=csv -p fieldlist="family|genus|specificepithet|infraspecificepithet"
