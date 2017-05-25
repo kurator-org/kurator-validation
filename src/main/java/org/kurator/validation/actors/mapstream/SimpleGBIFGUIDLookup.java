486c373b37c5cdd92c6a0e53560febe57ffd127e
Paul J. Morris <mole@morris.net>
2017-05-25 14:21:33 -0400

    ISSUE: PURPOSE: Get branching workflow to lookup taxon names.  DESCRIPTION: Adding in sci_name_qc library (with minimal GBIF and WoRMS lookups).  Added simple guid lookup actors for WoRMS and GBIF to the actors.mapstream package.  Included these actors in the dwca_branching_taxon_lookup workflow.

---
+++ src/main/java/org/kurator/validation/actors/mapstream/SimpleGBIFGUIDLookup.java
@@ -0,0 +1,54 @@
+/** SimpleGBIFLookup.java
+ *
+ * Copyright 2017 President and Fellows of Harvard College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kurator.validation.actors.mapstream;
+
+import java.util.Map;
+
+import org.filteredpush.qc.date.DateUtils;
+import org.filteredpush.qc.sciname.SciNameUtils;
+import org.kurator.akka.KuratorActor;
+
+/**
+ * @author mole
+ *
+ */
+public class SimpleGBIFGUIDLookup extends KuratorActor {
+
+	@Override
+	protected void onData(Object message) throws Exception {
+		if (message instanceof Map) {
+			try { 
+			   Map<String,String> record = (Map<String,String>) message;
+				if (record.containsKey("scientificName")) { 
+					String scientificName = record.get("scientificName");
+					String scientificNameAuthorship = record.get("scientificNameAuthorship");
+					String guid =  SciNameUtils.simpleGBIRGuidLookup(scientificName, scientificNameAuthorship);
+					if (!DateUtils.isEmpty(guid)) { 
+						if (DateUtils.isEmpty(record.get("taxonID"))) { 
+						    record.put("taxonID", guid);
+						}
+					}
+				}
+    		} catch (ClassCastException e) { 
+    			logger.error("Message was a Map, but unable to cast to Map<String,Stringt>.");
+    			logger.error(e.getMessage());
+    		}			   
+		} 
+		broadcast(message);
+	}
+	
+}
